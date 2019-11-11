0a34e4b71aaf2ada73370e86a3b00b4c1c6239cf
Gerson Groth <ge.groth@gmail.com>
2019-11-11 19:55:04 -0300

    allow headers

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ var routes = require('./api/routes/ecommerceRoutes'); //importing route
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-access-token");
   next();
 });
 
